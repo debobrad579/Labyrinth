e682cf12921a1c008bce85f69b22a84da6bd8888
NathanHartHDCH <NathanHart@hdch.org>
2021-03-02 18:57:17 -0500

    Changed coyote timer, added a bunch of comments (not done)

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,47 +1,97 @@
 extends KinematicBody2D
 
+# Export Constants
 export var ACCELERATION = 500
 export var MAX_SPEED = 90
 export var GRAVITY = 300
 export var JUMP_FORCE = 128
-export var FRICTION = 0.25
+export var FRICTION = 750
 export var AIR_RISISTANCE = 0.02
 export var WALL_SLIDE_ACCELERATION = 2
 export var MAX_WALL_SLIDE_SPEED = 30
 export var DOUBLE_JUMP_TOTAL = 1
 
+# Preload Nodes
 onready var floorDetector = $FloorDetector
-onready var jumpTimer = $Timer
+onready var CoyoteTimer = $CoyoteTimer
 onready var jumpTimer2 = $Timer2
 onready var moveTimer = $Timer3
 onready var moveTimer2 = $Timer4
 
+# Player Platforming Variables
 var motion = Vector2.ZERO
 var on_floor = false
-var jump = false
 var can_move = true
 var wall_jump = false
 var double_jump = DOUBLE_JUMP_TOTAL
 var wall_double_jump = true
 var can_resist = false
 
-func _physics_process(delta):
+# Main physics and process function
+func _physics_process(delta):\
+	
+	# Set the horizontal input
 	var x_input = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left")
+	
+	# If there is horizontal input
+	# Changed so that player cannot move "up walls" aka really steep slopes
+	if x_input != 0 and can_move == true and not is_on_wall():
 		
-	if x_input != 0 and can_move == true:
+		# Accelerate horizontally, clamp to max speed
 		motion.x += x_input * ACCELERATION * delta
 		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+	
+	# NOTE: -------------------------------------------------------------------
+	# I used the is_on_floor() function instead of the on_floor bool because it
+	# works better with the is_on_wall() variable... this way the player can't
+	# jump up high slopes. I also check if the coyote timer is out of time, so
+	# this implements the same functionality, just more efficiently.
+	# Finally, I removed the "jump" boolean and just set on_floor = false, which
+	# is now used to detect coyote time. --------------------------------------
+	
+	# If player is on floor,
+	if is_on_floor() == true or not CoyoteTimer.is_stopped():
 		
-	if on_floor == true:
+		# Set on_floor to true (this is used for detecting coyote time now
+		on_floor = true
+		
+		# TODO
+		ACCELERATION = 500
+		
+		# Wall jump is reset when on floor
 		wall_double_jump = true
 		double_jump = DOUBLE_JUMP_TOTAL
+		
+		# If there is not horizonal input (and on floor)
 		if x_input == 0:
-			motion.x = lerp(motion.x, 0, FRICTION)
+			
+			# 'Decelerate' horizontal motion by friciton
+			# NOTE: -----------------------------------------------------------
+			#move_toward just moves the value towards another value (in
+			# this case 0) by a set amount (continuously adds or subtracts to
+			# do so). Its very similar to the lerp function, however its
+			# actually more like the when we accelerate x. -------------------
+			motion.x = move_toward(motion.x, 0, FRICTION * delta)
+		
+		# If jump is pressed (and on floor)
 		if Input.is_action_just_pressed("jump"):
+			
+			# Set y velocity/motion to jump force (up) and on_floor to false.
 			motion.y = -JUMP_FORCE
-			jump = true
-			jumpTimer2.start()
+			on_floor = false
+
+	# If play is not on floor,
 	else:
+		
+		# If they were JUST on the floor,
+		if on_floor == true:
+			
+			# Start the coyote timer!
+			CoyoteTimer.start()
+			# Then indicate, they are no longer on the floor, so the coyote
+			# Timer is not reset again.
+			on_floor = false
+		
 		if Input.is_action_just_released("jump") and motion.y < -JUMP_FORCE/2 and wall_jump == false:
 			motion.y = -JUMP_FORCE/2
 			
@@ -60,6 +110,7 @@ func _physics_process(delta):
 		if Input.is_action_pressed("walk_right") and motion.x < 0 or Input.is_action_pressed("walk_left") and motion.x > 0:
 			can_move = true
 		if Input.is_action_just_pressed("jump") and Input.is_action_pressed("walk_right"):
+			ACCELERATION = 500
 			motion.y = -JUMP_FORCE
 			motion.x = -450
 			can_move = false
@@ -82,27 +133,37 @@ func _physics_process(delta):
 		if motion.y >= 0:
 			motion.y = min(motion.y + WALL_SLIDE_ACCELERATION, MAX_WALL_SLIDE_SPEED)
 	
-	motion.y += GRAVITY * delta
-	motion = move_and_slide(motion)
+	
+	var gravity_vector = Vector2(0, GRAVITY)
+	motion += gravity_vector * delta
+	motion = move_and_slide(motion, -gravity_vector, true, 4, PI/4, false)
 
 func _on_FloorDetector_body_entered(_body):
 	on_floor = true
 	wall_jump = false
 
 func _on_FloorDetector_body_exited(_body):
-	if jump == false:
-		jumpTimer.start()
-	else:
-		on_floor = false
+	pass
+	#if jump == false:
+	#	CoyoteTimer.start()
+	#else:
+	#	on_floor = false
 
 func _on_Timer_timeout():
-	on_floor = false
+	#on_floor = false
+	#print(rand_range(1,10))
+	
+	pass
 
 func _on_Timer2_timeout():
-	jump = false
+	#jump = false
+	
+	pass
 
 func _on_Timer3_timeout():
 	can_move = true
+	moveTimer2.start()
+	ACCELERATION = 200
 
 func _on_Timer4_timeout():
-	pass
+	ACCELERATION = 500
