e804549b528dd4118eeee46bd912af62002bd377
NathanHartHDCH <n.bernard.hart@gmail.com>
2021-03-07 14:09:19 -0500

    Fixed AI and line of sight.

--- Enemies/FlyingEnemyAI.gd
+++ Enemies/FlyingEnemyAI.gd
@@ -13,6 +13,19 @@ enum{
 
 var motion = Vector2.ZERO
 var state = IDLE
+# NEW VARIABLES:
+# Controls when the pursuit timer is turned on or off. You should change this 
+# to use the same mechanism that you used for the WanderController?
+var pursuit_timer_active = false
+# Since multiple players can play, this allows enenmies to target specific
+# players. I have programmed it to chase the first player to enter the area.
+# CHECK THE PLAYER DETECTION script!!
+var target_player = null
+# Aim is different. This will hold the relative difference between
+# The monsters location and the players. For example, if the player 
+# is 50px to the right and 10px down (FROM THE MONSTER), then the aim would be
+# that... not the players global poisiton, which could be anything.
+var aim = Vector2.ZERO
 
 onready var stats = $Stats
 onready var playerDetection = $PlayerDetection
@@ -20,8 +33,24 @@ onready var hurtbox = $Hurtbox
 onready var wanderController = $WanderController
 onready var objectDetector = $ObjectDetector
 
+# NEW PRELOADS:
+onready var pursuitTimer = $PursuitTimer
+# Will attempt to load in Player1 and Player2 from the current scene. If
+# Neither are present in the scene, they will return "null". (This is important
+# for single player mode
+onready var player1 = get_node_or_null('../Player1')
+onready var player2 = get_node_or_null('../Player2')
+
 func _ready():
 	state = pick_random_state([IDLE, WANDER])
+	
+	# NOTE!! ----------------------------------------------------------------------
+	# Due to a weird bug, the raycast won't initialize properly until the first
+	# cast change, meaning if this isn't included, then the first time a player
+	# enters the monster radius, even if there is a wall in the way, it will trigger
+	# the chase scene before recognizing the wall. This somehow fixes that. -------
+	if player1 != null: # <-- This is just to make sure that a crash error doesn't happen if the player doesn't load
+		objectDetector.cast_to = player1.position - position
 # Start at a random state.
 
 func _physics_process(delta):
@@ -52,21 +81,38 @@ func _physics_process(delta):
 		# will then choose a random state.
 			
 		CHASE:
-			var player = playerDetection.player
-			if player != null:
-				accelerate_towards_point(player.global_position, delta)
-			else:
-				state = IDLE
-		# The enemy will move towards the player if the player is detected.
+			# Added seek_player() in here, just so the monster keeps
+			# checking line of sight.
+			seek_player()
+			
+			# If the object detector is colliding, but the state is still
+			# CHASE, then check the timer
+			if objectDetector.is_colliding():
+				# If the timer hasn't started yet, start it.
+				if pursuit_timer_active == false:
+					pursuitTimer.start()
+					pursuit_timer_active = true
+					
+				# if the timer has started, but run out, then switch states.
+				# This gives the monster a 3 second pursuit length, but this
+				# can be changed under the 'PursuitTimer' node.
+				elif pursuit_timer_active == true and pursuitTimer.is_stopped():
+					pursuit_timer_active = false
+					state = pick_random_state([IDLE, WANDER])
+			
+			# NOTE THE AIM! Aim is the relative position from the monster, so
+			# One the player moves out of the 'line of sight' aim is perserved
+			# as the last location, so the monster keeps moving in that direction.
+			# Note that this acts like the monster keeps moving in a given
+			# direction. I can explain this more if you want.
+			accelerate_towards_point(position + aim, delta)
 				
 	motion = move_and_slide(motion)
+	
 
 func accelerate_towards_point(point, delta):
 	var direction = global_position.direction_to(point)
-	objectDetector.cast_to = direction * 8.5
 	motion = motion.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
-	if objectDetector.is_colliding():
-		motion = direction * global_position - point
 	
 func change_state():
 	state = pick_random_state([IDLE, WANDER])
@@ -74,9 +120,34 @@ func change_state():
 # A random state between "IDLE", and "WANDER" is chosen then 
 # the timer is reset to a time between 1 and 3 seconds.
 
+# THIS FUNCTION IS THE OTHER MAJOR THING I CHANGED AND IS WHERE LINE
+# OF SIGHT HAPPENS!
 func seek_player():
 	if playerDetection.can_see_player():
-		state = CHASE
+	
+		# So if a player is detected, then it will check which
+		# player is the first on the player detection list.
+		# This is then assigned as the "target_player".
+		if player1 == playerDetection.players[0]:
+			target_player = player1
+		elif player2 == playerDetection.players[0]:
+			target_player = player2
+		
+		# Cast the raycast at the target player (position is different from
+		# cast_to. Cast_to is the tip relative to position. In this case
+		# We want to move the tip, not position.
+		# Note that the cast_to is set to the players position relative to the 
+		# monsters. Without the - position (minus monster's position) the
+		# tip will not go to the player, but to the player + the monsters position
+		# from the origin.
+		objectDetector.cast_to = target_player.position - position
+		
+		# If not colliding, set aim and chase.
+		if not objectDetector.is_colliding():
+			# Aim is the relative position from the monster, aka the cast_to
+			# position. They are the same.
+			aim = objectDetector.cast_to
+			state = CHASE
 		
 func pick_random_state(state_list):
 	state_list.shuffle()
--- ReusableNodes/PlayerDetection.gd
+++ ReusableNodes/PlayerDetection.gd
@@ -1,12 +1,17 @@
 extends Area2D
 
-var player = null
+# I have changed it to use a list!!
+# This means that multiple players can be tracked at once.
+# If the player enters, they are added to the list/array
+# When they exit, they are removed.
+# The monster can_see_player whenever the list has contents (i.e. at least one person)
+var players = []
 
 func can_see_player():
-	return player != null
+	return players != []
 
 func _on_PlayerDetection_body_entered(body):
-	player = body
+	players.append(body)
 
 func _on_PlayerDetection_body_exited(body):
-	player = null
+	players.erase(body)
