303167b279f19637442b167b4f42b0a01d25258e
Brady <thebradster7.bd@gmail.com>
2021-03-14 23:14:17 -0400

    Added settings page

--- UI/TitleScreen/SettingsPage.gd
+++ UI/TitleScreen/SettingsPage.gd
@@ -5,8 +5,8 @@ var start_button = false
 func _physics_process(_delta):
 	if start_button == false:
 		if Input.is_action_just_pressed("ui_up"):
-			$Menu/Rows/Buttons/Play.grab_focus()
+			$Menu/Rows/LeftButtons/Difficulty.grab_focus()
 			start_button = true
 		if Input.is_action_just_pressed("ui_down"):
-			$Menu/Rows/Buttons/Settings.grab_focus()
+			$Menu/Rows/LeftButtons/Back.grab_focus()
 			start_button = true
