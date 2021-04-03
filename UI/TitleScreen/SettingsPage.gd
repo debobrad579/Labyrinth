e087bdcb5de0e47467fa55e73f6dd019d59906b7
Brady <thebradster7.bd@gmail.com>
2021-04-03 16:24:01 -0400

    Changed func _physics_process(delta), to func _input(event)

--- UI/TitleScreen/SettingsPage.gd
+++ UI/TitleScreen/SettingsPage.gd
@@ -2,11 +2,15 @@ extends Control
 
 var start_button = false
 
-func _physics_process(_delta):
-	if start_button == false:
-		if Input.is_action_just_pressed("ui_up"):
-			$Menu/Rows/LeftButtons/Difficulty.grab_focus()
-			start_button = true
-		if Input.is_action_just_pressed("ui_down"):
-			$Menu/Rows/LeftButtons/Back.grab_focus()
-			start_button = true
+func _input(event):
+	if event:
+		if start_button == false:
+			if Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_left"):
+				$Menu/Rows/LeftButtons/Difficulty.grab_focus()
+				start_button = true
+			if Input.is_action_just_pressed("ui_down"):
+				$Menu/Rows/RightButtons/Back.grab_focus()
+				start_button = true
+			if Input.is_action_just_pressed("ui_right"):
+				$Menu/Rows/RightButtons/Sound.grab_focus()
+				start_button = true
--- UI/TitleScreen/TitleScreen.gd
+++ UI/TitleScreen/TitleScreen.gd
@@ -2,11 +2,12 @@ extends Control
 
 var start_button = false
 
-func _physics_process(_delta):
-	if start_button == false:
-		if Input.is_action_just_pressed("ui_up"):
-			$Menu/Rows/Buttons/Play.grab_focus()
-			start_button = true
-		if Input.is_action_just_pressed("ui_down"):
-			$Menu/Rows/Buttons/Settings.grab_focus()
-			start_button = true
+func _input(event):
+	if event:
+		if start_button == false:
+			if Input.is_action_just_pressed("ui_up"):
+				$Menu/Rows/Buttons/Play.grab_focus()
+				start_button = true
+			if Input.is_action_just_pressed("ui_down"):
+				$Menu/Rows/Buttons/Settings.grab_focus()
+				start_button = true
